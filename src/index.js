4f32ed6d2e461f7f018ca2ea70852020cc3d9f43
03PrakharSrivastava <127031677+03PrakharSrivastava@users.noreply.github.com>
2024-04-13 03:40:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
+import './index.css';
+import import NavBar from './NavBar';
 
-
-ReactDOM.render(App(),
+ReactDOM.render(<>
+  <App />
+  <NavBar />
+  </>,
   document.getElementById('root')
 );
