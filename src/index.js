509e157ba4be85876e5f34f689c41b9131a652f5
03PrakharSrivastava <127031677+03PrakharSrivastava@users.noreply.github.com>
2024-04-14 17:58:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
 import NavBar from './NavBar';
+import HomePage from './HomePage';
+import StudentHome from './StudentHome';
 import './index.css';
 
 ReactDOM.render(<>
-  <App />
-  <NavBar />
+ <StudentHome 
+    type='student'
+    username='prakhar123'
+    recentQuizzes={[1,2,3,4]}
+  />
   </>,
   document.getElementById('root')
 );
